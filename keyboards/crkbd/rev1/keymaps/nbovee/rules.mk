2410bad327ed1dd45801b5715b57ffa1dbc70a7a
nbovee <12849851+nbovee@users.noreply.github.com>
2024-06-02 07:23:42 +0000

    prog

--- keyboards/crkbd/rev1/keymaps/nbovee/rules.mk
+++ keyboards/crkbd/rev1/keymaps/nbovee/rules.mk
@@ -1,9 +1,14 @@
-MCU = rp2040_ce
-BOOTLOADER = rp2040
-
+DEFAULT_FOLDER = crkbd/rev1
+CONVERT_TO=elite_pi
+# PIN_COMPATIBLE = elite_c
 OLED_ENABLE = yes
-
+# OLED_DRIVER = SSD1306
+# I2C_DRIVER = I2CD1
+# I2C1_SDA_PIN = GP2
+# I2C1_SCL_PIN = GP3
 RGBLIGHT_ENABLE = yes
+# RGBLIGHT_DRIVER = sk6812
 RGB_MATRIX_ENABLE = yes
-RGB_MATRIX_CUSTOM_USER = yes
+# RGB_MATRIX_DRIVER = is31fl3731
+# RGB_MATRIX_CUSTOM_USER = yes
 # To use custom effects in your code, simply prepend RGB_MATRIX_CUSTOM_ to the effect name specified in RGB_MATRIX_EFFECT().
